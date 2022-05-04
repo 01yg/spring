fca1e01c0ac4e8f325b230c642b8208231ad3808
yg <01.yg.jung@gmail.com>
2022-05-04 21:56:23 +0900

    실제 크롤링 -> 데이터 조합 -> 인서트 까지 하는 부분 한글로 주석을 담

--- src/main/java/com/example/web/controller/api/InvestingApi.java
+++ src/main/java/com/example/web/controller/api/InvestingApi.java
@@ -19,15 +19,20 @@ public class InvestingApi {
     @GetMapping("/postCall")
     @ResponseBody
     public void call() {
+        // 인베스팅 api 순서도
+        // 1. 컨트롤러 getInvestings() 크롤링하여 주식 정보 가져오기, 각각의 컨트롤러안에서 예외는 처리함
+        // 2. 컨트롤러 insertTechnical() 크롤링하여 점수 가져오기, 각각의 컨트롤러안에서 예외는 처리함
+        // 3. 실제로 dao.insert() 를 하는 것이 좋을듯
+        // . 컨트롤러 updateDataTheDayBefor() 하루전 데이터 업데이트 함.. << 업데이트 보다 조회해서 합치는게 좋을거 같고 일단 이건 보류혀자
         System.out.println("ctrl.insert()");
         HashMap<String, InvestingDTO> investings = ctrl.getInvestings();
         if(investings != null) {
             System.out.println("ctrl.insertTechnical()");
             investings = ctrl.insertTechnical(investings);
-            if(investings != null) {
-                System.out.println("ctrl.updateDataTheDayBefore()");
-                ctrl.updateDataTheDayBefore(investings);
-            }
+            //if(investings != null) {
+            //    System.out.println("ctrl.updateDataTheDayBefore()");
+            //    ctrl.updateDataTheDayBefore(investings);
+            //}
         }
     }
 
