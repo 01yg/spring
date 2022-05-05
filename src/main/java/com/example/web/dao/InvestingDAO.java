445313a7f3f127ff5bb015063db00dbcebe62127
yg <01.yg.jung@gmail.com>
2022-05-05 11:33:28 +0900

    HashMap 매핑이 잘 안되서 ArrayList로 변경

--- src/main/java/com/example/web/dao/InvestingDAO.java
+++ src/main/java/com/example/web/dao/InvestingDAO.java
@@ -3,12 +3,10 @@ package com.example.web.dao;
 import com.example.web.dto.InvestingDTO;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 public interface InvestingDAO {
-    int insertInvesting(HashMap<String, InvestingDTO> investings);
-    int updateInvestingTechnical(ArrayList<InvestingDTO> investings);
+    int insertInvesting(ArrayList<InvestingDTO> investings);
     int updateDataTheDayBefore(int i);
     List<InvestingDTO> select();
 }
