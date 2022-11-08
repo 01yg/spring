261d78c327908220ca9e20d9cdb4f1dbd4459951
LAPTOP-RLVQOL88\ygjung <wjd0r@naver.com>
2022-11-09 07:32:13 +0900

    매일 종목코드 새로 업뎃

--- src/main/java/com/example/web/controller/schedule/ScheduleController.java
+++ src/main/java/com/example/web/controller/schedule/ScheduleController.java
@@ -3,6 +3,7 @@ package com.example.web.controller.schedule;
 import com.example.web.controller.api.DaumApi;
 import com.example.web.controller.api.HolidayApi;
 import com.example.web.controller.api.InvestingApi;
+import com.example.web.controller.api.KrxApi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -15,9 +16,14 @@ public class ScheduleController {
     @Autowired
     InvestingApi investingApi;
 
+    @Autowired
+    KrxApi krxApi;
+
     // 8:59
     @Scheduled(cron = "0 56 8 * * MON-FRI", zone="Asia/Seoul")
     public void schedule1() {
+        // 이슈코드 생성
+        krxApi.call();
         // 오늘 날짜 테이블 생성
         investingApi.createTable();
         // 홀리데이 초기화
@@ -31,14 +37,14 @@ public class ScheduleController {
             // 스케쥴러 순서도
             // 1. isHoliday 휴일 체크
             // 2. 인베스팅 api 호출
-            investingApi.call();
+            //investingApi.call();
         }
     }
     // 9:00 ~ 14:59 25
     @Scheduled(cron = "0 0-20 15 * * MON-FRI", zone="Asia/Seoul")
     public void schedule3() {
         if(!DaumApi.isHoliday) {
-            investingApi.call();
+            //investingApi.call();
         }
     }
 
