1052f64930465ae431ab1ecbdd9fc488ef134ccf
ygjung <ygjung@iochord.com>
2022-11-11 08:23:44 +0900

    네이버 api 테스트용으로 만들어봄

---
+++ src/main/java/com/example/web/controller/api/NaverApi.java
@@ -0,0 +1,31 @@
+package com.example.web.controller.api;
+
+import com.example.web.http.NaverHttp;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/naver")
+@CrossOrigin(origins = "http://113.131.152.55:3000")
+public class NaverApi {
+    @GetMapping("/item")
+    @ResponseBody
+    public Map item(
+            @RequestParam(value="isuName", required = false) String isuName,
+            @RequestParam(value="periodType", required = false) String periodType,
+            @RequestParam(value="range", required = false) String range) {
+        System.out.println(isuName);
+        System.out.println(periodType);
+        System.out.println(range);
+
+        JSONObject jsonObject = NaverHttp.get("https://api.stock.naver.com/chart/domestic/item/035720" + //KrxApi.isu.get(isuName) +
+                "?periodType=" + periodType +
+                "&range=" + range);
+
+        return jsonObject.toMap();
+    }
+}
