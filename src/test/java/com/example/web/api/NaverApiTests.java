fc707c79a3ab2007d3fc8469be5db68ecbd755d6
ygjung <ygjung@iochord.com>
2022-11-11 08:24:18 +0900

    네이버 api테스트 인데 계속 실패했었음

---
+++ src/test/java/com/example/web/api/NaverApiTests.java
@@ -0,0 +1,13 @@
+package com.example.web.api;
+
+import com.example.web.controller.api.NaverApi;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public class NaverApiTests {
+    @Test
+    void naverItem() throws Exception {
+    }
+}
